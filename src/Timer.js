39594bba8efaa629400d77da1e9c12bac9de80c7
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-15 12:59:39 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -24,8 +24,6 @@ class Timer extends Component {
 
   componentDidUpdate() {
     this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
-    this.timer.current.style.width = 240+this.state.time*5/1000+"px"
-    this.timer.current.style.height = 150+this.state.time*5/1000+"px"
   }
 
 
